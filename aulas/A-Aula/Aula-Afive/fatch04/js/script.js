311ed33bf33ed0210f90118a64feb777472bc505
Vitor-Alves17 <vitoralves3306@gmail.com>
2026-03-05 10:27:49 -0300

    feat: created chose activity

--- aulas/A-Aula/Aula-Afive/fatch04/js/script.js
+++ aulas/A-Aula/Aula-Afive/fatch04/js/script.js
@@ -1,32 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const produtoForm = document.getElementById('form')
-    const produTb = document.getElementById('tb')
-    this.getElementsByTagName('tbody')[0];
+    const tbody = document.getElementsByTagName('tbody')[0];
     const produtoIdEl = document.getElementById('produtoId')
     const limparP = document.getElementById('buttony')
 
     let edit = false;
     function getProdutos() {
         const produtos = localStorage.getItem('produtos')
-        return produtos ? JSON.parse('produtos') : [];
-
-        function saveProduto() {
+        return produtos ? JSON.parse(produtos) : [];
+    }
+        function saveProduto(produtos) {
             localStorage.setItem('produtos', JSON.stringify(produtos));
         }
-        function deleteProduto() {
-            produTb.innerHTML = '';
+        function showProduto() {
+            tbody.innerHTML = '';
 
             const produtos = getProdutos();
-            for (let i = o; i < produtos.length; i++) {
+            for (let i = 0; i < produtos.length; i++) {
                 const produto = produtos[i];
 
-                const row = produtoTb.insertRow();
+                const row = tbody.insertRow();
 
                 const nomeCell = row.insertCell();
                 nomeCell.textContent = produto.nome;
 
                 const precoCell = row.insertCell();
-                precoCell.textContent = `R$${produto.preco.toFixed(2)}`;
+                precoCell.textContent = 'R$ ' + produto.preco;
 
                 const disponiCell = row.insertCell();
                 disponiCell.textContent = produto.disponi;
@@ -37,27 +36,71 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 const editBtn = document.createElement('button');
                 editBtn.textContent = 'Editar';
-                editBtn.onclick = () => edititarProduto(i);
+                editBtn.onclick = () => editarProduto(i);
                 actionCell.appendChild(editBtn);
 
                 const deleteBtn = document.createElement('button');
-                editBtn.textContent = 'Deletar';
-                editBtn.onclick = () => deleteProduto();
-                actionCell.appendChild(editBtn);
+                deleteBtn.textContent = 'Deletar';
+                deleteBtn.onclick = () => removeProduto();
+                actionCell.appendChild(deleteBtn);
 
             }
         }
-        produtoForm.addEventListener('submit', (event) => {
-            event.preventDefault();
 
-            const nome = document.getElementById('nome').value;
-            const preco = perseFloat(document.getElementById('preco').value);
-            const disponivel = document.getElementById('disponi').velue
-            const prudutoId = produtoIdEl.value;
+    produtoForm.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-            if (nome && !isNaN(preco)) {
+        const nome = document.getElementById('nome').value;
+        const preco = parseFloat(document.getElementById("preco").value)
+        const disponivel = document.getElementById('disponi').value
+        const produtoId = produtoIdEl.value;
 
+        if(nome && !isNaN(preco)){
+            const produtos = getProdutos();
+
+            if (edit) {
+                produtos[produtoId].nome = nome;
+                produtos[produtoId].preco = preco;
+                produtos[produtoId].disponibilidade = disponivel;
+                edit = false;
+            } else {
+                produtos.push( {nome: nome, preco: preco, disponi: disponivel } );
             }
-        });
+            saveProduto(produtos);
+            showProduto();
+            produtoForm.reset();
+            produtoIdEl.value = "";
+        } else {
+            alert('pero le falta sazon')
+        }
+    })
+
+
+    function editarProduto(index) {
+        edit = true;
+        const produtos = getProdutos();
+        const produto = produtos[index];
+
+        document.getElementById('nome').value = produto.nome;
+        document.getElementById('preco').value = produto.preco;
+        document.getElementById('disponi').value = produto.disponibilidade;
+
+        limparP.style.display = 'inline-block';
     }
+
+    function removeProduto(index) {
+        if(confirm('Are you sure?')) {
+            const produtos = getProdutos();
+            produtos.splice(index, 1)
+            saveProduto(produtos);
+            showProduto();
+        }
+    }
+
+    limparP.addEventListener('click', () => {
+        edit = false;
+        produtoForm.reset();
+        produtoIdEl.value = '';
+        limparP.style.display = 'none';
+    })
 })
