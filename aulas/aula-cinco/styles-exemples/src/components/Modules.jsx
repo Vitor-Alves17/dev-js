78aa222a725b45d57e590905cc9bf8cb0c6b43c7
Vitor-Alves17 <vitoralves3306@gmail.com>
2026-03-24 09:55:36 -0300

    feat: created stylies review

--- aulas/aula-cinco/styles-exemples/src/components/Modules.jsx
+++ aulas/aula-cinco/styles-exemples/src/components/Modules.jsx
@@ -3,7 +3,15 @@ import btn from './ModuleStyle.module.css'
 export default function Modules() {
     return (
         <>
-            <button className={btn.diva}>Divas unidas jamis serão vencidas</button>
+            <div className={btn.card}>
+                <label htmlFor="slc">Escolha uma cor</label>
+                <select name="slc" id={btn.slc}>
+                    <option value="">azul</option>
+                    <option value="">vermelho</option>
+                    <option value="">verde</option>
+                </select>
+                <button className={btn.diva}>Escolher</button>
+            </div>
         </>
     )
 }
--- aulas/aula-cinco/styles-exemples/src/components/Tradicional.jsx
+++ aulas/aula-cinco/styles-exemples/src/components/Tradicional.jsx
@@ -3,7 +3,17 @@ import './TradicionalStyle.css'
 export default function Tradicional() {
     return (
         <>
-            <button>Dives unides</button>
+            <div className="card">
+                <p>Escolha um trimestre</p>
+                <div className="form">
+                    <label htmlFor="tri">Trimestres</label>
+                    <select name="tri" id="tri">
+                    <option value="1">1 - Janeiro a Março</option>
+                    <option value="2">2 - Abril a Junho</option>
+                    <option value="3">3 - Julho a Setembro</option>
+                    <option value="4">4 - Outubro a Dezembro</option>
+                </select></div>
+            </div>
         </>
     )
 }
