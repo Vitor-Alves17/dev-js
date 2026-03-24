606b1f839a935783f54f7d5a5b0b1f3c25e1b57c
Vitor-Alves17 <vitoralves3306@gmail.com>
2026-03-24 11:04:46 -0300

    feat: created stylies review

--- aulas/aula-cinco/styles-exemples/src/components/Styled-C.jsx
+++ aulas/aula-cinco/styles-exemples/src/components/Styled-C.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
+import {useState} from "react";
 
-const Buttone = styled.button`
+const Sel = styled.select`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -10,8 +11,14 @@ const Buttone = styled.button`
     padding: 10px;
 `
 
+
+
+const Op = styled.option`
+    background-color: ${props => props.color};
+`
+
 const Card = styled.div`
-    background-color: aliceblue;
+    background-color: ${props => props.color};
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -24,11 +31,20 @@ const Card = styled.div`
 `
 
 export default function Style_c() {
+    const [color, setColor] = useState('')
+
     return (
         <>
-            <Card>
-                <p>Veja nossos produtos</p>
-                <Buttone>Divos esquecidos</Buttone>
+            <Card color={color}>
+                <p>Escolha uma cor</p>
+
+                {/*usetate tem que ser criado dentro de um recat components, pra usar ele no styles em outro lugar fora desse react components, usamos props para atribuir o valor)*/}
+
+                <Sel>
+                    <Op color={color} onClick={() => setColor('red')}>{'Red'}</Op>
+                    <Op color={color} onClick={() => setColor('blue')}>{'Blue'}</Op>
+                    <Op color={color} onClick={() => setColor('green')}>{'Green'}</Op>
+                </Sel>
             </Card>
         </>
     )
