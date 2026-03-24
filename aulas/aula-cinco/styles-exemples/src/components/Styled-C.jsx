4cb6eaac7ea18a0349ad52ad16c4e7705491d83a
Vitor-Alves17 <vitoralves3306@gmail.com>
2026-03-24 08:27:49 -0300

    feat: created card styled-components

--- aulas/aula-cinco/styles-exemples/src/components/Styled-C.jsx
+++ aulas/aula-cinco/styles-exemples/src/components/Styled-C.jsx
@@ -10,10 +10,26 @@ const Buttone = styled.button`
     padding: 10px;
 `
 
+const Card = styled.div`
+    background-color: aliceblue;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: start;
+    gap: 20px;
+    border: solid 1px black;
+    border-radius: 10px;
+    padding: 30px;
+    width: fit-content;
+`
+
 export default function Style_c() {
     return (
         <>
-          <Buttone>Divos esquecidos</Buttone>
+            <Card>
+                <p>Veja nossos produtos</p>
+                <Buttone>Divos esquecidos</Buttone>
+            </Card>
         </>
     )
 }
