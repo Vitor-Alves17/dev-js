febc127acaf6bcf0dca94487ccdf83b5e88762b3
Vitor-Alves17 <vitoralves3306@gmail.com>
2026-05-28 11:09:06 -0300

    feat: created dragon ball api project

--- DragonBallApi/ApiDragonBall/src/components/main/index.jsx
+++ DragonBallApi/ApiDragonBall/src/components/main/index.jsx
@@ -1,9 +1,16 @@
+import './style.css'
 import {Card} from "../card/index.jsx";
 
 export const Main = () => {
     return (
-        <>
+        <main>
             <Card url="gay" name="gay" description="gay" />
-        </>
+            <Card url="gay" name="gay" description="gay" />
+            <Card url="gay" name="gay" description="gay" />
+            <Card url="gay" name="gay" description="gay" />
+            <Card url="gay" name="gay" description="gay" />
+            <Card url="gay" name="gay" description="gay" />
+            <Card url="gay" name="gay" description="gay" />
+        </main>
     )
 }
