3a345e4e36bcc676a92ffb090a07496774d8b435
Vitor-Alves17 <vitoralves3306@gmail.com>
2026-06-11 10:08:54 -0300

    feat: finished dragon ball api project

--- DragonBallApi/ApiDragonBall/src/components/main/index.jsx
+++ DragonBallApi/ApiDragonBall/src/components/main/index.jsx
@@ -1,16 +1,47 @@
 import './style.css'
 import {Card} from "../card/index.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export const Main = () => {
+
+    const [characters, setCharacters] = useState([]);
+
+    // useEffect(() => {
+    //    fetch("https://dragonball-api.com/api/characters?limit=58")
+    //        .then(res => res.json())
+    //     .then(data => {
+    //         setCharacters(data.items);
+    //         console.log(data.items)
+    //     })
+    //        .catch(err => console.log(err));
+    // }, []);
+
+    useEffect(() => {
+        axios.get("https://dragonball-api.com/api/characters?limit=58")
+        .then(res => {
+            setCharacters(res.data.items)
+        })
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <main>
-            <Card url="gay" name="gay" description="gay" />
-            <Card url="gay" name="gay" description="gay" />
-            <Card url="gay" name="gay" description="gay" />
-            <Card url="gay" name="gay" description="gay" />
-            <Card url="gay" name="gay" description="gay" />
-            <Card url="gay" name="gay" description="gay" />
-            <Card url="gay" name="gay" description="gay" />
+            <div className="card-wrapper">
+                {characters.map((character) => {
+                    return (
+                        <Card url={character.image} name={character.name} description={character.description} />
+                        )
+                })}
+            </div>
+
+            {/*<Card url={characters[0].image} name={characters[0].name} description={characters[0].description} />*/}
+            {/*/!*<Card url="gay" name="gay" description="gay" />*!/*/}
+            {/*/!*<Card url="gay" name="gay" description="gay" />*!/*/}
+            {/*/!*<Card url="gay" name="gay" description="gay" />*!/*/}
+            {/*/!*<Card url="gay" name="gay" description="gay" />*!/*/}
+            {/*/!*<Card url="gay" name="gay" description="gay" />*!/*/}
+            {/*/!*<Card url="gay" name="gay" description="gay" />*!/*/}
         </main>
     )
 }
